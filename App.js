8c72554f462273ffd539b1b6036d91dc857254c1
marwa <marwajomaa4@gmail.com>
2020-06-15 15:42:59 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
+  const deleteItem = (key) => {
+    setTodos((PrevTodos) => {
+      return PrevTodos.filter((item) => item.key !== key);
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} deleteItem={deleteItem} />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
---
+++ components/todoItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function todoItem({ item, deleteItem }) {
+  return (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => {
+        deleteItem(item.key);
+      }}
+    >
+      <Text>{item.text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
+});
